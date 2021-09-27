8e34bbf7f6890977b3a030a3de81ba6fb31e617e
2-js <80288048+2-js@users.noreply.github.com>
2021-09-26 20:26:40 -0500

    Update fetched.js

--- fetched.js
+++ fetched.js
@@ -1,2 +1,2 @@
-var r = window.onbeforeunload = function() { return "lock" }; if (r != null) { var c = 'locked'} else { var c = 'not locked' } alert ( 'tab '+ c )
+var r = window.onbeforeunload = function() { return "lock" }; if (r != null) { var c = 'locked'} else { var c = 'not locked' } alert ( window.location.origin+ ' is ' + c )
 //// locks tab by getting the 'onbeforeunload' then making it return 'lock' what this does is when you usally exit the tab it has no return value so it can exit or refresh without a waring
