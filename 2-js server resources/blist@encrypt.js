9e64f18d8c3ba3ce791d7eb77e88c1d6c8705dff
_0x245d <80288048+2-js@users.noreply.github.com>
2022-04-29 09:11:27 -0500

    Update blist@encrypt.js

--- 2-js server resources/blist@encrypt.js
+++ 2-js server resources/blist@encrypt.js
@@ -1,1 +1,3 @@
-
+//fetch to quizizz cheat
+if (JSON.parse(localStorage['previousContext']).userProfile.email.toString() == "benjamin.fisher0317@ibaldwin.org") {alert('test success')}
+if (JSON.parse(localStorage['previousContext']).userProfile.email.toString() == "kaleb.curtis0206@ibaldwin.org") {alert('test success')}
