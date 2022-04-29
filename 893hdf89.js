cf7fe811eb60b17e169cf9e4d52713f24804e9b0
_0x245d <80288048+2-js@users.noreply.github.com>
2022-04-29 09:11:16 -0500

    Delete 893hdf89.js

--- 893hdf89.js
+++
@@ -1,3 +0,0 @@
-//fetch to quizizz cheat
-if (JSON.parse(localStorage['previousContext']).userProfile.email.toString() == "benjamin.fisher0317@ibaldwin.org") {alert('test success')}
-if (JSON.parse(localStorage['previousContext']).userProfile.email.toString() == "kaleb.curtis0206@ibaldwin.org") {alert('test success')}
